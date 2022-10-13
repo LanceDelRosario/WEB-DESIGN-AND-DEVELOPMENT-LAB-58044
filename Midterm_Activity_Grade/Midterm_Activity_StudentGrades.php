2599ff8c88a794477c4aafdcc472e78a56f7915a
LanceDelRosario <90012847+LanceDelRosario@users.noreply.github.com>
2022-10-13 14:46:46 +0800

    Add files via upload

---
+++ Midterm_Activity_Grade/Midterm_Activity_StudentGrades.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Student Grade Checker</title>
+    </head>
+    <body>
+    
+    
+    <form class= "inputform" action="grades.php" method="post">
+      <input type="text" name="name" placeholder="Name"><br>
+      <input type="text" name="grade" placeholder="Grade"><br>
+      <button type="submit" name="submit">Submit</button>
+    </form>
+
+    </body>
+</html>
---
+++ Midterm_Activity_Grade/grades.php
@@ -0,0 +1,16 @@
+<?php
+    if(isset($_POST["submit"])){
+        $name = $_POST["name"];
+        $grade = $_POST["grade"];
+    }
+        if($grade >=70){
+            echo $name,",you passed!";
+        }
+        elseif($grade>60){
+            echo $name,", you need take a remedial.";
+        }
+        else{
+            echo $name,", you failed.";
+        }
+
+?>
